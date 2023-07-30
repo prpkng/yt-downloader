95a2ba0bab7e0f888d6e89ba184ff22e1ae3f911
Purpkin <guiejo2020@gmail.com>
2023-07-30 17:23:29 -0300

    update

--- netlify/functions/server.ts
+++ netlify/functions/server.ts
@@ -1,11 +1,16 @@
 import express, { Router } from 'express';
+import path from 'path';
 import serverless from 'serverless-http';
 
 const api = express();
 
 const router = Router();
-router.get('/hello', (req, res) => res.send('Hello World!'));
+router.get('/', (req, res) => res.sendFile(path.join(__dirname+'/index.html')));
 
 api.use('/', router);
 
+
+api.listen()
+
 export const handler = serverless(api);
+
